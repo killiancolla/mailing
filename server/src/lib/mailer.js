ceab4ad86ac4a84d6c54ce4c5be9b13972748b80
killiancolla <killiancolla@hotmail.fr>
2025-03-21 10:14:36 +0100

    fix unsub

--- server/src/lib/mailer.js
+++ server/src/lib/mailer.js
@@ -74,7 +74,7 @@ export async function sendEmail(lead, mail) {
             subject: subject,
             html: finalBody.replace(/\n/g, '<br>'),
             headers: {
-                "List-Unsubscribe": `<${process.env.FRONT_URL}/unsubscribe/${lead.id}>, <mailto:unsubscribe@killian-colla.com?subject=unsubscribe>`,
+                "List-Unsubscribe": `<${process.env.FRONT_URL}/unsubscribe/${lead.id}>, <mailto:contact@killian-colla.com?subject=unsubscribe>`,
                 "List-Unsubscribe-Post": "List-Unsubscribe=One-Click"
             }
         };
